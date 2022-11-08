60778f1ac48347eaf33f870856808dbad0887ab9
yup-ma <89696198+yup-ma@users.noreply.github.com>
2022-11-09 02:34:32 +0530

    Quotes final version

--- Cotroller/JS/Func/Sharing.js
+++ Cotroller/JS/Func/Sharing.js
@@ -99,11 +99,9 @@ function cumulativeSharingFunc(url, heading) {
         document.body.removeEventListener('click', sharingDropdownClosingFunc)
     })
     document.querySelector(".share-btn").addEventListener('click', sharingDropdownOpenFunc)
-    console.log(whatsappUrl, heading)
 }
 
 function articleBlocksSharingFunc(container, url, heading) {
-    // console.log(container, url, heading)
     heading = heading.replace(`"`, `'`)
     let subjectVar = `${extraDetail} - ${heading} on Quality Reads`;
     let bodyVar = `${hashtag}%0A${heading} @QualityReads%0A${url}%0A%0A${sharingDesc}`;
@@ -196,6 +194,90 @@ function articleBlocksSharingFunc(container, url, heading) {
         document.body.removeEventListener('click', sharingDropdownClosingFunc)
     })
     container.querySelector(".close-share-option").addEventListener('click', () => {
+        document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
+        document.body.removeEventListener('click', sharingDropdownClosingFunc)
+    })
+}
+
+
+function quoteBlocksSharingFunc(container, heading, author) {
+    let url = "https://yup-ma.github.io/Quality-Reads/Quotes.html"
+    heading = heading.replace(`"`, `'`)
+    let subjectVar = `Checkout Quotes on Quality Reads`;
+    let bodyVar = `Quote: ${heading} - ${author}  %0A@QualityReads%0A${url}`;
+    let emailUrl = `mailto:?subject=${encodeURIComponent(subjectVar)}&Body=${encodeURIComponent(bodyVar).replace(/%25/g, '%')}`;
+    let whatsappUrl = `https://wa.me?text=Checkout Quotes: ${heading} - ${author}  on Quality Reads%0A${url}`
+
+    if (navigator.share) {
+        container.innerHTML = `<span class="dropdown-type-text">Share</span>
+        <a href="${whatsappUrl}" target="_blank" class="share-option whatsapp-share-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 448">
+                <path d="M380.9,97.1A220.329,220.329,0,0,0,223.9,32c-122.4,0-222,99.6-222,222A222.239,222.239,0,0,0,31.5,365L0,480l117.7-30.9a221.324,221.324,0,0,0,106.1,27h.1c122.3,0,224.1-99.6,224.1-222,0-59.3-25.2-115-67.1-157Zm-157,341.6a184.191,184.191,0,0,1-94-25.7l-6.7-4L53.4,427.3,72,359.2l-4.4-7A183.908,183.908,0,0,1,39.4,254C39.4,152.3,122.2,69.5,224,69.5a182.88,182.88,0,0,1,130.4,54.1c34.8,34.9,56.2,81.2,56.1,130.5C410.5,355.9,325.6,438.7,223.9,438.7ZM325.1,300.5c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5,2.8s-14.3,18-17.6,21.8c-3.2,3.7-6.5,4.2-12,1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8,5.7-9.1,16.3-30.3,1.8-3.7.9-6.9-.5-9.7s-12.5-30.1-17.1-41.2c-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2a20.549,20.549,0,0,0-14.8,6.9c-5.1,5.6-19.4,19-19.4,46.3s19.9,53.7,22.6,57.4c2.8,3.7,39.1,59.7,94.8,83.8,35.2,15.2,49,16.5,66.6,13.9,10.7-1.6,32.8-13.4,37.4-26.4s4.6-24.1,3.2-26.4C334.3,304.6,330.6,303.2,325.1,300.5Z" transform="translate(0 -32)" fill="currentColor"/>
+            </svg>
+            <span class="share-option-text">WhatsApp</span>
+        </a>
+        <a href="${emailUrl}" class="share-option email-share-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 512 384">
+                <path d="M48,64a48,48,0,0,0-28.8,86.4L236.8,313.6a32.1,32.1,0,0,0,38.4,0L492.8,150.4A48,48,0,0,0,464,64H48ZM0,176V384a64.059,64.059,0,0,0,64,64H448a64.059,64.059,0,0,0,64-64V176L294.4,339.2a63.9,63.9,0,0,1-76.8,0Z" transform="translate(0 -64)" fill="currentColor"/>
+            </svg>
+            <span class="share-option-text">Email</span>
+        </a>
+        <button class="share-option copy-link-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 604.35 470.31">
+                <path d="M579.8,267.7A144.6,144.6,0,0,0,393.5,47.8l-1.6,1.1a31.968,31.968,0,0,0,37.2,52l1.6-1.1A80.6,80.6,0,0,1,534.5,222.4L422.3,334.8A80.6,80.6,0,0,1,299.7,231l1.1-1.6a31.968,31.968,0,0,0-52-37.2l-1.1,1.6A144.519,144.519,0,0,0,467.5,380ZM60.2,244.3A144.6,144.6,0,0,0,246.5,464.2l1.6-1.1a31.968,31.968,0,0,0-37.2-52l-1.6,1.1A80.633,80.633,0,0,1,105.5,289.5L217.7,177.2A80.627,80.627,0,0,1,340.3,281.1l-1.1,1.6a31.968,31.968,0,1,0,52,37.2l1.1-1.6A144.551,144.551,0,0,0,172.5,132Z" transform="translate(-17.825 -20.845)" fill="currentColor"/>
+            </svg>
+            <span class="share-option-text">Copy link</span>
+        </button>
+        <button class="share-option more-share-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 112 432">
+                <path d="M64,360A56,56,0,1,1,8,416,56.036,56.036,0,0,1,64,360Zm0-160A56,56,0,1,1,8,256,56.036,56.036,0,0,1,64,200ZM120,96A56,56,0,1,1,64,40,56.036,56.036,0,0,1,120,96Z" transform="translate(-8 -40)" fill="currentColor"/>
+            </svg>
+            <span class="share-option-text">More option</span>
+        </button>`;
+        container.querySelector(".more-share-option").addEventListener('click', () => {
+            navigator.share({
+                title: subjectVar,
+                url: url
+            })
+            document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
+            document.body.removeEventListener('click', sharingDropdownClosingFunc)
+        })
+    } else {
+        container.innerHTML = `<span class="dropdown-type-text">Share</span>
+        <a href="${whatsappUrl}" target="_blank" class="share-option whatsapp-share-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 448 448">
+                <path d="M380.9,97.1A220.329,220.329,0,0,0,223.9,32c-122.4,0-222,99.6-222,222A222.239,222.239,0,0,0,31.5,365L0,480l117.7-30.9a221.324,221.324,0,0,0,106.1,27h.1c122.3,0,224.1-99.6,224.1-222,0-59.3-25.2-115-67.1-157Zm-157,341.6a184.191,184.191,0,0,1-94-25.7l-6.7-4L53.4,427.3,72,359.2l-4.4-7A183.908,183.908,0,0,1,39.4,254C39.4,152.3,122.2,69.5,224,69.5a182.88,182.88,0,0,1,130.4,54.1c34.8,34.9,56.2,81.2,56.1,130.5C410.5,355.9,325.6,438.7,223.9,438.7ZM325.1,300.5c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5,2.8s-14.3,18-17.6,21.8c-3.2,3.7-6.5,4.2-12,1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8,5.7-9.1,16.3-30.3,1.8-3.7.9-6.9-.5-9.7s-12.5-30.1-17.1-41.2c-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2a20.549,20.549,0,0,0-14.8,6.9c-5.1,5.6-19.4,19-19.4,46.3s19.9,53.7,22.6,57.4c2.8,3.7,39.1,59.7,94.8,83.8,35.2,15.2,49,16.5,66.6,13.9,10.7-1.6,32.8-13.4,37.4-26.4s4.6-24.1,3.2-26.4C334.3,304.6,330.6,303.2,325.1,300.5Z" transform="translate(0 -32)" fill="currentColor"/>
+            </svg>
+            WhatsApp</span>
+        </a>
+        <a href="${emailUrl}" class="share-option email-share-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 512 384">
+                <path d="M48,64a48,48,0,0,0-28.8,86.4L236.8,313.6a32.1,32.1,0,0,0,38.4,0L492.8,150.4A48,48,0,0,0,464,64H48ZM0,176V384a64.059,64.059,0,0,0,64,64H448a64.059,64.059,0,0,0,64-64V176L294.4,339.2a63.9,63.9,0,0,1-76.8,0Z" transform="translate(0 -64)" fill="currentColor"/>
+            </svg>
+            Email</span>
+        </a>
+        <button class="share-option copy-link-option d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 604.35 470.31">
+                <path d="M579.8,267.7A144.6,144.6,0,0,0,393.5,47.8l-1.6,1.1a31.968,31.968,0,0,0,37.2,52l1.6-1.1A80.6,80.6,0,0,1,534.5,222.4L422.3,334.8A80.6,80.6,0,0,1,299.7,231l1.1-1.6a31.968,31.968,0,0,0-52-37.2l-1.1,1.6A144.519,144.519,0,0,0,467.5,380ZM60.2,244.3A144.6,144.6,0,0,0,246.5,464.2l1.6-1.1a31.968,31.968,0,0,0-37.2-52l-1.6,1.1A80.633,80.633,0,0,1,105.5,289.5L217.7,177.2A80.627,80.627,0,0,1,340.3,281.1l-1.1,1.6a31.968,31.968,0,1,0,52,37.2l1.1-1.6A144.551,144.551,0,0,0,172.5,132Z" transform="translate(-17.825 -20.845)" fill="currentColor"/>
+            </svg>
+            Copy link</span>
+        </button>`;
+    }
+    container.querySelector(".copy-link-option").addEventListener('click', () => {
+        actionStatus = "success";
+        actionMessage = "Link copied to clipboard";
+        showActionMessageFunc();
+        navigator.clipboard.writeText(url)
+        document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
+        document.body.removeEventListener('click', sharingDropdownClosingFunc)
+    })
+    container.querySelector(".whatsapp-share-option").addEventListener('click', () => {
+        window.open(`${container.querySelector(".whatsapp-share-option").href}`, '_blank');
+        document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
+        document.body.removeEventListener('click', sharingDropdownClosingFunc)
+    })
+    container.querySelector(".email-share-option").addEventListener('click', () => {
+        window.location.href = container.querySelector(".email-share-option").href;
         document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
         document.body.removeEventListener('click', sharingDropdownClosingFunc)
     })
--- Cotroller/JS/Quotes.js
+++ Cotroller/JS/Quotes.js
@@ -1,24 +1,23 @@
 let quotesAPIUrl = "https://type.fit/api/quotes"
 
-// apiRunningFunc()
+apiRunningFunc()
 //Running the api for quotes
 function apiRunningFunc() {
     document.querySelector(".quotes-main-container").innerHTML = `<div class="loader-container">
-    <div>
-        <span class="loader loader1">l</span>  
-        <span class="loader loader2">o</span>  
-        <span class="loader loader3">a</span>  
-        <span class="loader loader4">d</span>  
-        <span class="loader loader5">i</span>  
-        <span class="loader loader6">n</span>  
-        <span class="loader loader7">g</span>  
-    </div>
-</div>`;
+        <div>
+            <span class="loader loader1">l</span>  
+            <span class="loader loader2">o</span>  
+            <span class="loader loader3">a</span>  
+            <span class="loader loader4">d</span>  
+            <span class="loader loader5">i</span>  
+            <span class="loader loader6">n</span>  
+            <span class="loader loader7">g</span>  
+        </div>
+    </div>`;
     fetch(quotesAPIUrl)
         .then(response => response.json())
         .then((jsonData) => {
-            console.log(jsonData)
-            // creatingArticlesFunc(jsonData.hits, jsonData.page, jsonData.nbPages, jsonData.nbHits, jsonData.processingTimeMS);
+            creatingQuotesFunc(jsonData);
         })
         //Handling errors
         .catch((error) => {
@@ -28,3 +27,107 @@ function apiRunningFunc() {
             </div>`
         });
 }
+
+
+//Creating search results based on data from api
+function creatingQuotesFunc(quotesArray) {
+    document.querySelector(".quotes-main-container").innerHTML = "";
+    if (quotesArray !== 0) {
+        let randomNumber = Math.floor(Math.random() * 100);
+        let quoteText = quotesArray[randomNumber].text;
+        let quoteAuthor = quotesArray[randomNumber].author;
+        if (quoteAuthor == null || quoteAuthor == "") {
+            quoteAuthor = "Unknown"
+        }
+        const newDiv = document.createElement("div");
+        newDiv.className = "quotes-container d-flex d-flex-dir-col";
+        newDiv.innerHTML = `<button class="bookmark-quote d-flex justify-content-center" title="Bookmark Quote">
+        <span class="option-icon d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 143.93 191.886">
+                <path id="Path_176" data-name="Path 176"
+                    d="M125.938,0H17.991A17.991,17.991,0,0,0,0,17.991V179.874a11.993,11.993,0,0,0,18.036,10.36l53.929-31.462L125.9,190.231c7.984,4.337,18.029-1.1,18.029-10.356V17.991A17.993,17.993,0,0,0,125.938,0Zm0,169.417L71.965,137.932,17.991,169.417V20.24A2.18,2.18,0,0,1,19.9,17.991H123.352a2.315,2.315,0,0,1,2.586,2.249Z"
+                    fill="currentColor"></path>
+            </svg>
+        </span>
+        <span class="hidden-ele">Bookmark Quote</span>
+    </button>
+    <div class="quote-text">${quoteText}</div>
+    <div class="quote-author">- ${quoteAuthor}</div>
+    <div class="quote-bottom-container">
+        <div>
+            <div class="quote-share-container d-flex d-flex-dir-col">
+                <button class="share-btn option-btn d-flex">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 448 448">
+                        <path
+                            d="M352,224a96,96,0,1,0-96-96,93.886,93.886,0,0,0,.7,11.9l-94.1,47a96,96,0,1,0,0,138.2l94.1,47A92.8,92.8,0,0,0,256,384a96.071,96.071,0,1,0,29.4-69.1l-94.1-47a101.5,101.5,0,0,0,0-23.8l94.1-47A95.237,95.237,0,0,0,352,224Z"
+                            transform="translate(0 -32)" fill="currentColor"></path>
+                    </svg>
+                    Share
+                </button>
+                <div class="share-options-container d-flex d-flex-dir-col">
+                </div>
+            </div>
+        </div>
+        <button class="new-quote-btn option-btn d-flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 170.703 159.35">
+                <path
+                    d="M61.13,71.415a56.893,56.893,0,0,1,80.124-.356L126.6,85.675a8.54,8.54,0,0,0,6.046,14.581h45.521a8.515,8.515,0,0,0,8.535-8.535V46.2a8.54,8.54,0,0,0-14.581-6.046L157.328,54.949A79.651,79.651,0,0,0,26.242,85.106a11.372,11.372,0,1,0,21.445,7.575A56.32,56.32,0,0,1,61.13,71.415ZM16,131.552v45.521a8.54,8.54,0,0,0,14.581,6.046l14.794-14.794A79.737,79.737,0,0,0,176.5,138.2a11.372,11.372,0,0,0-21.445-7.575,56.32,56.32,0,0,1-13.443,21.267,56.893,56.893,0,0,1-80.124.356L76.1,137.6a8.54,8.54,0,0,0-6.046-14.581H24.535A8.515,8.515,0,0,0,16,131.552Z"
+                    transform="translate(-16 -31.962)" fill="currentColor" />
+            </svg>
+            New Quote
+        </button>
+    </div>`;
+        let randomArticleColor = randomColorGenerator();
+        newDiv.style.setProperty("--quote-color", `rgb(${randomArticleColor})`);
+        newDiv.style.setProperty("--quote-bg-color", `${randomArticleColor}`);
+
+        document.querySelector(".quotes-main-container").appendChild(newDiv);
+        newDiv.querySelector(".bookmark-quote").addEventListener('click', addToBookmarkFunc)
+        newDiv.querySelector(".share-btn").addEventListener('click', shareOptionsOpenerFunc)
+        newDiv.querySelector(".new-quote-btn").addEventListener('click', apiRunningFunc)
+        quoteBlocksSharingFunc(newDiv.querySelector(".share-options-container"), quotesArray[randomNumber].text, quotesArray[randomNumber].author)
+    } else {
+        document.querySelector(".quotes-main-container").innerHTML = `<div class="fetch-api-error-container d-flex justify-content-center">
+            <img src="View/Images/empty-list-image.svg" alt="Couldn't find any results">
+            <p>Opps, Couldn't find any article<br>Don't worry<br>Try out different <a href="#search-input-parent-container">filters</a> and <a href="#search-input-parent-container">search query</a>
+        </div>`;
+    }
+}
+
+function shareOptionsOpenerFunc() {
+    if (document.querySelector(".active-share-dropdown")) {
+        document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
+    }
+    this.parentElement.classList.add("active-share-dropdown");
+    document.body.addEventListener('click', sharingDropdownClosingFunc)
+}
+// Closing dropdowns for filters
+function sharingDropdownClosingFunc(e) {
+    let currentTarget = document.querySelector(".active-share-dropdown").contains(e.target);
+    if (!currentTarget) { //clikced outside of box
+        document.querySelector(".active-share-dropdown").classList.remove("active-share-dropdown");
+        document.body.removeEventListener('click', sharingDropdownClosingFunc)
+    }
+}
+
+function addToBookmarkFunc() {
+    if (this.classList.contains("active-bookmark")) {
+        this.classList.remove("active-bookmark")
+        this.querySelector(".option-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 143.93 191.886">
+            <path id="Path_176" data-name="Path 176"
+                d="M125.938,0H17.991A17.991,17.991,0,0,0,0,17.991V179.874a11.993,11.993,0,0,0,18.036,10.36l53.929-31.462L125.9,190.231c7.984,4.337,18.029-1.1,18.029-10.356V17.991A17.993,17.993,0,0,0,125.938,0Zm0,169.417L71.965,137.932,17.991,169.417V20.24A2.18,2.18,0,0,1,19.9,17.991H123.352a2.315,2.315,0,0,1,2.586,2.249Z"
+                fill="currentColor" />
+        </svg>`;
+    } else {
+        this.classList.add("active-bookmark")
+        this.querySelector(".option-icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 88.056 117.408">
+            <path d="M0,11.007V111.836A5.582,5.582,0,0,0,8.783,116.4L44.028,91.725,79.274,116.4a5.582,5.582,0,0,0,8.783-4.563V11.007A11.01,11.01,0,0,0,77.049,0H11.007A11.01,11.01,0,0,0,0,11.007Z"
+                fill="currentColor" />
+        </svg>`;
+    }
+}
+
+document.querySelectorAll(".theme-dropdown-content-container .theme-btn").forEach(ele => {
+    ele.addEventListener('click', apiRunningFunc)
+});
+window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', apiRunningFunc);
